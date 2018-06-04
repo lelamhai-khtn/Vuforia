a721f694d5b2dd186c6edc2bab42944603bf98e2
lelamhai <leelamhair@gmail.com>
2018-06-04 21:20:59 +0700

    finish

---
+++ Assets/Resources/Scripts/Hole.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+    public GameObject mole;
+    public float aliveTime = 1f;
+
+    public int intervalMin = 2;
+    public int intervalMax = 10;
+	// Use this for initialization
+	void Start () {
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+    }
+	
+	void Spawn()
+    {
+        GameObject g = (GameObject)Instantiate(mole, transform.position, Quaternion.identity);
+        Destroy(g, aliveTime);
+        Invoke("Spawn", Random.Range(intervalMin, intervalMax));
+    }
+}
---
+++ Assets/Resources/Scripts/Mole.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour {
+    public GameObject onHit;
+    private void OnMouseDown()
+    {
+        GameObject g = (GameObject)Instantiate(onHit,transform.position,Quaternion.identity);
+        Destroy(g, 0.2f);
+    }
+}
